20c6aebfa0c5d15d0eb064d198af25746042e7fd
Shubhang Sharma <ss186133@ncr.com>
2022-07-14 13:07:56 -0400

    Initial Commit

---
+++ quickr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quickr
+//
+//  Created by Sharma, Sharma on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
